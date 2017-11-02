62da1c3211f99aa461db7178c810ce9c39fcc2fe
L117student <L117student@L117-18-043663.bitnet.wctc.edu>
2017-11-01 21:48:55 -0500

    Created First Calculation Method

--- src/student/lab/BMD_DateUtilites8.java
+++ src/student/lab/BMD_DateUtilites8.java
@@ -5,10 +5,22 @@
  */
 package student.lab;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
+import java.time.temporal.UnsupportedTemporalTypeException;
+
 /**
  *
  * @author L117student
  */
 public class BMD_DateUtilites8 {
     
+    
+    public final int getTemporalFieldBetween(LocalDateTime date1, LocalDateTime date2, ChronoField unit)
+                throws DateTimeException,UnsupportedTemporalTypeException,ArithmeticException {
+       if(date1.isAfter(date2)){LocalDateTime temp = date1;date1 = date2;date2 = temp;}//Swap Dates
+       return date1.get(unit) - date2.get(unit); 
+    }
 }
